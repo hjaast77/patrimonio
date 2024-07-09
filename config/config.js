10410b39d33ed231511721d69c7d221001abc2cd
Hugo Jastrzebski <hugo.jastrzebski@gmail.com>
2024-07-09 17:31:19 -0300

    db

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
-    host: "mysql-hjast.alwaysdata.net",
-    user: "hjast",
-    password: "cac2024",
-    database: "hjast_db",
+    host: "mysql-patrimoniouba.alwaysdata.net",
+    user: "367195_root",
+    password: "queliolia",
+    database: "patrimoniouba_db",
     port: 3306,
   },
 };
