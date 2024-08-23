c082f4a9ab2f19c89596130008b88ad05bfa3969
Hugo Jastrzebski <hugo.jastrzebski@gmail.com>
2024-08-23 00:11:09 -0300

    bar code scanning

--- public/js/scan.js
+++ public/js/scan.js
@@ -1,23 +1,20 @@
 function startScan() {
   const html5QrCode = new Html5Qrcode("scanner");
-
-  // Configure the scanner to focus on CODE_128 and other barcode types
-  const config = {
-    fps: 10, // Frames per second
-    qrbox: 250, // Optional, specify the dimensions of the scanning area
-    formatsToSupport: [Html5QrcodeSupportedFormats.CODE_128],
-  };
-
   html5QrCode
     .start(
-      { facingMode: "environment" }, // Use the back camera on mobile devices
-      config,
+      { facingMode: "environment" },
+      {
+        fps: 10, // Optional, frame per second for barcode scanning
+        qrbox: 250, // Optional, if you want bounded box UI
+      },
       (decodedText, decodedResult) => {
-        document.getElementById("bienInput").value = decodedText;
+        // Extract the last 5 digits from the decoded text
+        const lastFiveDigits = decodedText.slice(-5);
+        document.getElementById("bienInput").value = lastFiveDigits;
         html5QrCode.stop();
       },
       (errorMessage) => {
-        // Handle scanning failures
+        // Handle scanning failure
         console.error(errorMessage);
       }
     )
