6939fd28fcae6b92dd32afa3cfe80d3f5224e26c
Hugo <hugo.jastrzebski@gmail.com>
2024-08-23 11:31:33 -0300

    scan bar code 3

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   database: {
-    // host: "mysql-patrimoniouba.alwaysdata.net",
-    // user: "367195_root",
-    // password: "queliolia",
-    // database: "patrimoniouba_db",
+    host: "mysql-patrimoniouba.alwaysdata.net",
+    user: "367195_root",
+    password: "queliolia",
+    database: "patrimoniouba_db",
 
-    host: "mysql-hjast.alwaysdata.net",
-    user: "hjast",
-    password: "cac2024",
-    database: "hjast_db",
+    // host: "mysql-hjast.alwaysdata.net",
+    // user: "hjast",
+    // password: "cac2024",
+    // database: "hjast_db",
 
     // host: "localhost",
     // user: "root",
